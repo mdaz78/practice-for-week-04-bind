d509a673fdb2832caa50545e0c0dd35eeba04ee2
Md Abu Zafar <mdabuzafar78@gmail.com>
2024-05-19 06:43:14 +0530

    solve bind exercise

--- test.js
+++ test.js
@@ -1,1 +1,6 @@
-// Your code here
+const Employee = require("./employee.js");
+
+const emp1 = new Employee("John Wick", "Dog Lover");
+
+setTimeout(emp1.sayName.bind(emp1), 2000);
+setTimeout(emp1.sayOccupation.bind(emp1), 3000);
